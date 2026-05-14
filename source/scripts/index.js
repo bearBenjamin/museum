c604bdf53e74596bf95049cfa25c1e1d50716c49
Ilia Konev <122528494+bearBenjamin@users.noreply.github.com>
2026-05-14 11:56:37 +0300

    feat: написал логику для секции Ticket и поправил стили секции

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,11 +1,12 @@
 import { initMenu } from './burger-menu';
 import { initModal } from './modal-window';
+import { initSlider } from './slider-welcome';
+import { initExploreSlider } from './slider-explore';
 import { setInitialValueVideo } from './video';
 import { initCastomPlayer } from './video-player';
 import { initVideoSlider } from './slider-video';
 import { reshuffleGallery, initGalleryAnimation } from './gallery';
-import { initSlider } from './slider-welcome';
-import { initExploreSlider } from './slider-explore';
+import './form.js';
 
 initMenu();
 initSlider();
